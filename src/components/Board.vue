ef4fb6a2a3f1952f2881e50e157fa0c045a07837
sundlee <sundlee@gmail.com>
2020-01-26 00:38:50 +0900

    중첩 라우트

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,6 +2,19 @@
   <div>
     Board
     <div>bid: {{ bid }}</div>
+    <div>
+      Board List:
+      <ul>
+        <li>
+          <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+        </li>
+        <li>
+          <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+        </li>
+      </ul>
+    </div>
+    <hr />
+    <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import Home from '../components/Home.vue';
 import Login from '../components/Login.vue';
 import Board from '../components/Board.vue';
+import Card from '../components/Card.vue';
 import NotFound from '../components/NotFound.vue';
 
 
@@ -14,7 +15,11 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/login', component: Login },
-    { path: '/b/:bid', component: Board },
+    {
+      path: '/b/:bid', component: Board, children: [
+        { path: 'c/:cid', component: Card }
+      ]
+    },
     { path: '*', component: NotFound },
   ]
 });
