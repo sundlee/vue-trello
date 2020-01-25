a4425a65f5f4d8ac7814ee956bd5a4e81d1e4e6b
sundlee <sundlee@gmail.com>
2020-01-25 12:24:33 +0900

    라우터 뷰

---
+++ src/components/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Page not Found</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../components/Home.vue';
+import Login from '../components/Login.vue';
+import NotFound from '../components/NotFound.vue';
+
+
+Vue.use(VueRouter);
+
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home },
+    { path: '/login', component: Login },
+    { path: '*', component: NotFound },
+  ]
+});
+
+export default router;
