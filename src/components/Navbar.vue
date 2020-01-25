6de37fb54f71f0b858ae93a8b0ae14fdbc7c58b1
sundlee <sundlee@gmail.com>
2020-01-25 12:44:39 +0900

    라우터 링크

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <router-link to="/">home</router-link>
+    <router-link to="/login">login</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
