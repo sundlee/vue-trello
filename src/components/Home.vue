f46bfb357195f4d4ee683b67fd9302b3d3f12d41
sundlee <sundlee@gmail.com>
2020-01-26 09:47:30 +0900

    Ajax - HttpXMLRequst 객체

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,8 @@
     Home
     <div>
       Board List:
+      <div v-if="loading">Loading...</div>
+      <div v-else>Api result: {{ apiRes }}</div>
       <ul>
         <li>
           <router-link to="/b/1">Board 1</router-link>
@@ -16,7 +18,37 @@
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			loading: false,
+			apiRes: '',
+		};
+	},
+	created() {
+		this.fetchData();
+	},
+	methods: {
+		fetchData() {
+			this.loading = true;
+
+			const req = new XMLHttpRequest();
+
+			req.open('GET', 'http://localhost:3000/health');
+
+			req.send();
+
+			req.addEventListener('load', () => {
+				this.loading = false;
+				this.apiRes = {
+					status: req.status,
+					statusText: req.statusText,
+					response: JSON.parse(req.response),
+				};
+			});
+		},
+	},
+};
 </script>
 
 <style>
