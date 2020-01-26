910e82ddeb813bde8f4096749fdeb56d90e7800f
sundlee <sundlee@gmail.com>
2020-01-26 10:05:54 +0900

    axios로 변경

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,13 @@
     <div>
       Board List:
       <div v-if="loading">Loading...</div>
-      <div v-else>Api result: {{ apiRes }}</div>
+      <div v-else>
+        Api result:
+        <pre>{{ apiRes }}</pre>
+      </div>
+      <div v-if="error">
+        <pre>{{ error }}</pre>
+      </div>
       <ul>
         <li>
           <router-link to="/b/1">Board 1</router-link>
@@ -18,11 +24,14 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
 	data() {
 		return {
 			loading: false,
 			apiRes: '',
+			error: '',
 		};
 	},
 	created() {
@@ -32,20 +41,17 @@ export default {
 		fetchData() {
 			this.loading = true;
 
-			const req = new XMLHttpRequest();
-
-			req.open('GET', 'http://localhost:3000/health');
-
-			req.send();
-
-			req.addEventListener('load', () => {
-				this.loading = false;
-				this.apiRes = {
-					status: req.status,
-					statusText: req.statusText,
-					response: JSON.parse(req.response),
-				};
-			});
+			axios
+				.get('http://localhost:3000/health')
+				.then(res => {
+					this.apiRes = res.data;
+				})
+				.catch(res => {
+					this.error = res.response.data;
+				})
+				.finally(() => {
+					this.loading = false;
+				});
 		},
 	},
 };
