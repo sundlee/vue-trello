4afd0232a8f30c48bcc4a332fcbddbe2119912ab
sundlee <sundlee@gmail.com>
2020-02-01 08:40:40 +0900

    vuex - 보드 목록 조회

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,14 +17,14 @@
         <a class="new-board-btn" href @click.prevent="addBoard">Create new board...</a>
       </div>
     </div>
-    <AddBoard v-if="isAddBoard" @submit="onAddBoard" />
+    <AddBoard v-if="isAddBoard" />
   </div>
 </template>
 
 <script>
 import { board } from '../api';
 import AddBoard from './AddBoard.vue';
-import { mapState, mapMutations } from 'vuex';
+import { mapState, mapMutations, mapActions } from 'vuex';
 
 export default {
 	components: {
@@ -33,12 +33,11 @@ export default {
 	data() {
 		return {
 			loading: false,
-			boards: [],
 			error: '',
 		};
 	},
 	computed: {
-		...mapState(['isAddBoard']),
+		...mapState(['isAddBoard', 'boards']),
 	},
 	created() {
 		this.fetchData();
@@ -49,23 +48,17 @@ export default {
 		});
 	},
 	methods: {
-		...mapMutations([]),
+		...mapMutations(['IS_SET_ADD_BOARD']),
+		...mapActions(['FETCH_BOARDS']),
 		fetchData() {
 			this.loading = true;
-			board
-				.fetch()
-				.then(data => {
-					this.boards = data.list;
-				})
-				.finally(() => {
-					this.loading = false;
-				});
+			this.FETCH_BOARDS();
+			board.fetch().finally(() => {
+				this.loading = false;
+			});
 		},
 		addBoard() {
 			this.$store.commit('SET_IS_ADD_BOARD', true);
-		},
-		onAddBoard() {
-			this.fetchData();
 		},
 	},
 };
