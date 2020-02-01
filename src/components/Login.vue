32c829823980f644fcf29530aa20338550e9dfe7
sundlee <sundlee@gmail.com>
2020-02-01 10:44:07 +0900

    vuex - 인증 2

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -29,7 +29,7 @@
 </template>
 
 <script>
-import { auth, setAuthInHeader } from '../api';
+import { mapActions } from 'vuex';
 
 export default {
 	data() {
@@ -49,11 +49,12 @@ export default {
 		this.rPath = this.$route.query.rPath || '/';
 	},
 	methods: {
+		...mapActions([
+			'LOGIN',
+		]),
 		onSubmit() {
-			auth.login(this.email, this.password)
-				.then(data => {
-					localStorage.setItem('token', data.accessToken);
-					setAuthInHeader(data.accessToken);
+			this.LOGIN({ email: this.email, password: this.password })
+				.then(() => {
 					this.$router.push(this.rPath);
 				})
 				.catch(err => {
